fc978285ee126f7da7c6768e43035497a67f2ba4
Hasanov-B <boxa1998hasanov18@gmail.com>
2020-01-23 15:51:24 -0500

    add1-2

---
+++ src/test/java/com/automation/tests/day2/ExchangeRatesAPITest.java
@@ -0,0 +1,95 @@
+package com.automation.tests.day2;
+
+
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class ExchangeRatesAPITest {
+    private String baseURI = "http://api.openrates.io/";
+
+    @Test
+    public void test1() {
+        Response response = given().get(baseURI + "latest");
+        assertEquals(200, response.getStatusCode());
+        System.out.println(response.prettyPrint());
+
+    }
+
+
+    @Test
+    public void test2() {
+        Response response = given().get(baseURI + "latest");
+        // verify that content type is json
+        assertEquals(200, response.getStatusCode());
+        //verify that data coming as json
+        assertEquals("application/json", response.getHeader("Content-Type"));
+// or like this -->
+        assertEquals("application/json", response.getContentType());
+    }
+
+
+    @Test
+    public void test3() {
+        //#TASK: get currency exchange rate for dollar. By default it's euro.
+//        Response response = given().get(baseURI+"latest?base=USD");
+        Response response = given().baseUri(baseURI + "latest").
+                //  basePath("latest").
+                        queryParam("base", "USD").
+                        get();
+        assertEquals(200, response.getStatusCode());
+        System.out.println(response.prettyPrint());
+    }
+
+    // #Task verify that response body, for latest currency rate,contains tadays date
+    @Test
+    public void test4() {
+        Response response = given().
+                baseUri(baseURI + "latest").
+                queryParam("base", "GBP").
+                get();
+        String todaysDate = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        System.out.println("Today's date: " + todaysDate);
+        assertEquals(200, response.getStatusCode());
+        assertTrue(response.getBody().asString().contains(todaysDate));
+    }
+
+
+
+    @Test
+            public void test5(){
+        Response response= given().baseUri(baseURI+"history").
+                queryParam("start_at","2001-01-01").
+                queryParam("end_at","2001-12-31").
+                queryParam("base","USD").
+                queryParam ("symbols","EUR","GBP","JPY").get();
+        System.out.println(response.prettyPrint());
+
+
+
+
+
+
+
+
+
+
+    }
+
+    }
+
+
+
+
+
+
+
+
+
--- src/test/java/com/automation/tests/day2/ORDSTests.java
+++ src/test/java/com/automation/tests/day2/ORDSTests.java
@@ -1,5 +1,73 @@
 package com.automation.tests.day2;
+import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
 
-public class ORDSTests {
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+//address of ORDS web service, that is running no AWS ec2.
+//dara is coming from SQL Oracle data base to this web service
+//during back-end testing with SQL developer and JDBC API
+//we were accessing data base directly
+//now, we gonna access web service
 
+//according to OOP conventions, all instance variable should be private
+//but, if we will make it public, it will not make ant difference for us
+//it's just good practice, so later we will not hesitate which keyword to use when it's gonna important
+
+//ec2-34-201-69-55.compute-1.amazonaws.com - my host, you have something else
+//   /ords/hr or //ords/hr/employees - same for all
+
+public class ORDSTests  {
+
+private  String baseUrl="ec2-54-89-146-161.compute-1.amazonaws.com/ords/hr";
+    //we start from given()
+    //then we can specify type of request like: get(), put(), delete(), post()
+    //and as parameter, we enter resource location (URI)
+    //then we are getting response back. that response we can put into Response object
+    //from response object, we can retrieve: body, header, status code
+    //it works without RestAssured.given() because of static import
+    //verify that status code is 200
+    @Test
+    public void Test1(){
+        //header stands for meta data
+        //usually it's used to include cookies
+        //in this example, we are specifying what kind of response type we need
+        //because web service can return let's say json or xml
+        //when we put header info "Accept", "application/json", we are saying that we need only json as response
+Response response=given().
+             get("http://ec2-54-89-146-161.compute-1.amazonaws.com/employees/");
+        //System.out.println(response.getBody().asString());
+        assertEquals(200,response.getStatusCode());
+        System.out.println(response.prettyPrint());
+        System.out.println();
+    }
+@Test
+    public  void Test2(){
+        Response response=given().
+                header("Accept","application/json").
+                get("http://ec2-54-89-146-161.compute-1.amazonaws.com:1000/ords/hr/employees/100");
+        int actualCode=response.getStatusCode();
+    System.out.println(response.prettyPrint());
+    assertEquals(200
+            ,actualCode);
+
+    System.out.println("What kind of content server sends to you , in this response:"+response.getHeader("Content-Type"));
+    }
+
+
+    @Test
+    public void test3(){
+        Response response = given().get("http://ec2-54-89-146-161.compute-1.amazonaws.com:1000/ords/hr/regions");
+        assertEquals(200, response.getStatusCode());
+        //to get specific header
+        Header header = response.getHeaders().get("Content-Type");
+        //print all headers one by one
+        for(Header h: response.getHeaders()){
+            System.out.println(h);
+        }
+        System.out.println(response.prettyPrint());
+    }
 }
+
